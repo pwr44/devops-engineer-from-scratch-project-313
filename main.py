4babd7a7d53b88b6172a99c013868ffd4d719f6f
Pavel Egorov <epe.mw@mail.ru>
2025-11-15 11:39:43 +0300

    cors added.

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import logging
 
-app = FastAPI()
+app = FastAPI(title="api")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+origins = [
+    "http://localhost/ping",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/ping")
 def read_root():
+    logging.info("GET '/ping'")
     return "pong"
